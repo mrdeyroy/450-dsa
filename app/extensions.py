87d9adbcaa8c6971041ee615a1ebbe8acf96ef48
mrdeyroy <shibamdeyroy02@gmail.com>
2026-05-22 22:39:54 +0530

    local-run

--- app/extensions.py
+++ app/extensions.py
@@ -8,7 +8,18 @@ from werkzeug.local import LocalProxy
 from flask_caching import Cache
 
 
-mongo = PyMongo()
+from flask_pymongo import PyMongo
+import mongomock
+
+class MockPyMongo(PyMongo):
+    def init_app(self, app, uri=None, *args, **kwargs):
+        super().init_app(app, uri, *args, **kwargs)
+        self.cx = mongomock.MongoClient()
+        uri = uri or app.config.get("MONGO_URI")
+        db_name = uri.split("/")[-1].split("?")[0] if uri else "450_dsa"
+        self.db = self.cx[db_name]
+
+mongo = MockPyMongo()
 db = LocalProxy(lambda: mongo.db)
 bcrypt = Bcrypt()
 login_manager = LoginManager()
